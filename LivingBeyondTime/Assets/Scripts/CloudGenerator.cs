2732a85a172fd3cf0cb8cd79d5a39c5f65cb3c53
D-Zidane-G <dtgarcia2@student.apc.edu.ph>
2021-10-09 13:36:05 +0800

    Added Parallax Background & Cloud Generator

---
+++ LivingBeyondTime/Assets/Scripts/CloudGenerator.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] clouds;
+
+    [SerializeField]
+    float spawnInterval;
+
+    [SerializeField]
+    GameObject endPoint;
+
+    Vector3 startPos;
+
+    public int StartingClouds;
+
+    void Start()
+    {
+        startPos = transform.position;
+        Prewarm();
+        Invoke("AttemptSpawn", spawnInterval);
+    }
+
+    void SpawnCloud(Vector3 startPos)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, clouds.Length);
+        GameObject cloud = Instantiate(clouds[randomIndex]);
+
+        float startY = UnityEngine.Random.Range(startPos.y - 1f, startPos.y + 1f);
+        cloud.transform.position = new Vector3(startPos.x, startY, startPos.z);
+
+        float scale = UnityEngine.Random.Range(0.8f, 1.2f);
+        cloud.transform.localScale = new Vector2(scale, scale);
+
+        float speed = UnityEngine.Random.Range(0.5f, 1.5f);
+        cloud.GetComponent<CloudScript>().StartFloating(speed, endPoint.transform.position.x);
+
+    }
+
+    void AttemptSpawn()
+    {
+        SpawnCloud(startPos);
+
+        Invoke("AttemptSpawn", spawnInterval);
+    }
+
+    void Prewarm()
+    {
+        for (int i = 0; i < StartingClouds; i++)
+        {
+            Vector3 spawnPos = startPos + Vector3.left * (i * 2);
+            SpawnCloud(spawnPos);
+        }
+    }
+}
