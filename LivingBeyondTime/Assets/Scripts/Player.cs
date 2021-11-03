10836a8be75a0a95f97baed9212f7ff5c1ffaa34
duanematyu <dsrodriguez@student.apc.edu.ph>
2021-11-03 19:36:57 +0800

    Walk Animation

--- LivingBeyondTime/Assets/Scripts/Player.cs
+++ LivingBeyondTime/Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     [Header("Configs")]
     public PlayerController2D controller;
     public float runSpeed;
+    public Animator playerAnimator;
+    public Rigidbody2D playerRigidbody;
 
     float horizontalMove = 0f;
     public bool jump = false;
@@ -14,18 +16,31 @@ public class Player : MonoBehaviour
     [Header("Inventory")]
     public int Keys = 0;
 
+    private enum State { idle, running, jumping }
+    private State state = State.idle;
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         //transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
         horizontalMove = Input.GetAxis("Horizontal") * runSpeed;
+       
 
         if (Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
         {
             jump = true;
+            //state = State.jumping;
         }
 
         UpdateKey();
+        VelocityState();
+        playerAnimator.SetInteger("state", (int)state);
     }
 
     void FixedUpdate()
@@ -37,6 +52,24 @@ public class Player : MonoBehaviour
     public void UpdateKey()
     {
 
+    }
+
+    private void VelocityState()
+    {
+        if(state == State.jumping)
+        {
+
+        }
+
+        else if(Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon)
+        {
+            state = State.running;
+        }
+
+        else
+        {
+            state = State.idle;
+        }
     }
 
 }
