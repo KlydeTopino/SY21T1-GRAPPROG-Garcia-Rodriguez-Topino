5ffe79161389255e324041790a8cdf44aa6961ae
duanematyu <dsrodriguez@student.apc.edu.ph>
2021-11-09 14:37:51 +0800

    Player Combat and Enemy Taking Damage

---
+++ LivingBeyondTime/Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator enemyAnimator;
+
+    public int maxHearts = 3;
+    int currentHearts;
+
+    public float destroyDelay = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHearts = maxHearts;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHearts -= damage;
+
+        enemyAnimator.SetTrigger("Hurt");
+
+        if (currentHearts <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy Died");
+
+        enemyAnimator.SetBool("IsDead", true);
+
+        this.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(gameObject, destroyDelay);
+    }
+}
---
+++ LivingBeyondTime/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator playerAnimator;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange = .5f;
+    public int attackDamage = 1;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+
+    public float constant = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= nextAttackTime) //Checks if it exceeds the nextAttackTime so the player can attack again
+        {
+            if (Input.GetButtonDown("Attack"))
+            {
+                Attack();
+                nextAttackTime = Time.time + constant / attackRate; // interval of attacks to avoid spamming the attack button
+            }
+        }
+    }
+
+    void Attack()
+    {
+        //Trigger Attack Animation
+        playerAnimator.SetTrigger("SpearAttack");
+        //Determines the range of the attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
